3899f32e92a0f3acfa422e0a055f46eedb82bedf
anjumshaikh <taamulani25@gmail.com>
2023-08-19 14:18:53 +0530

    my project

--- style.js
+++ style.js
@@ -1,18 +1,18 @@
-let url="https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+let ulr="https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
 const fetchDataPromise = () => {
     try {
         fetch(url)
         .then((response) => {
-            console.log(response.json());
+             console.log(response.json());
             return response.json();
         })
         .then((result)=>{
             console.log(result);
             const allnames=result.map((element)  =>{
-                return element.type;
+                return element.name;
             } )
             console.log(allnames);
-            
+          
         })
     .catch((error) => { 
         console.log(error);
@@ -23,3 +23,4 @@ const fetchDataPromise = () => {
     console.log(error);
    }
 }
+
