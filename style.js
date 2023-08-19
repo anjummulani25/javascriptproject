ef34cd3aa1cc2e35e0390ff092026c197486489f
anjumshaikh <taamulani25@gmail.com>
2023-08-19 14:23:17 +0530

    modified file

--- style.js
+++ style.js
@@ -1,13 +1,13 @@
-let ulr="https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+let url="https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
 const fetchDataPromise = () => {
     try {
         fetch(url)
         .then((response) => {
-             console.log(response.json());
+             
             return response.json();
         })
         .then((result)=>{
-            console.log(result);
+            //console.log(result);
             const allnames=result.map((element)  =>{
                 return element.name;
             } )
